eec60de363126c9af5ea9d560c1da1e2d4add751
Neville Neville <nchallinger@gmail.com>
2017-07-05 18:09:21 +1000

    Add webpack favicon build

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   entry: path.join(__dirname, '/client/main.js'),
@@ -27,6 +28,9 @@ module.exports = {
     ]
   },
   plugins: [
-    new ExtractTextPlugin('styles.css')
+    new ExtractTextPlugin('styles.css'),
+    new HtmlWebpackPlugin({
+      favicon: 'favicon.ico'
+    })
   ]
 }
