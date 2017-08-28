c8ae8f3f383d3035ef6c3968e033eda600b95c59
Neville Neville <nchallinger@gmail.com>
2017-08-28 14:16:20 +1000

    Add click functionality to storemarker.

--- client/components/geo/StoreMarker.js
+++ client/components/geo/StoreMarker.js
@@ -3,9 +3,13 @@ import React from 'react'
 class StoreMarker extends React.Component {
   constructor (props) {
     super(props)
-    this.state = {hover: false}
+    this.state = {
+      hover: false,
+      popup: false
+    }
     this.handleMouseIn = this.handleMouseIn.bind(this)
     this.handleMouseOut = this.handleMouseOut.bind(this)
+    this.handleClick = this.handleClick.bind(this)
   }
   handleMouseIn () {
     this.setState({hover: true})
@@ -13,37 +17,17 @@ class StoreMarker extends React.Component {
   handleMouseOut () {
     this.setState({hover: false})
   }
+  handleClick () {
+    this.setState({hover: false, popup: !this.state.popup})
+  }
   render () {
-    const storeDotStyle = {
-      backgroundImage: 'url("./style/images/hl_icon2.png")',
-      backgroundRepeat: 'no-repeat',
-      width: '22px',
-      height: '22px',
-      borderRadius: '16px',
-      border: '1px solid rgb(230, 157, 50)',
-      cursor: 'pointer',
-      zIndex: 1
-    }
-
-    const infoPopStyle = {
-      width: '125px',
-      height: '60px',
-      background: 'white',
-      border: '1px solid rgb(230, 157, 50)',
-      position: 'absolute',
-      top: '-60px',
-      left: '10px',
-      fontSize: '0.92em',
-      color: 'black',
-      display: this.state.hover ? 'block' : 'none',
-      zIndex: 1000
-    }
     return (
       <div style={{position: 'relative'}}>
         <div
           className='store-dot'
           onMouseOver={this.handleMouseIn}
           onMouseOut={this.handleMouseOut}
+          onClick={this.handleClick}
           />
         <div
           className='store-info'
@@ -51,10 +35,17 @@ class StoreMarker extends React.Component {
           <div>{this.props.store.name}</div>
           <div>{this.props.store.address}</div>
         </div>
+        <div
+          className='store-info'
+          style={{display: this.state.popup ? 'block' : 'none'}}
+          onClick={this.handleClick}
+          >
+          <div>{this.props.store.name}</div>
+          <div>{this.props.store.address}</div>
+        </div>
       </div>
     )
   }
 }
-
 
 export default StoreMarker
