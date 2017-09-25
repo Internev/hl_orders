d7314edf0958079fe9802a219be43516fcaa6d6e
Neville Neville <nchallinger@gmail.com>
2017-09-25 14:33:05 +1000

    Update store geo get query to include comment field.

--- server/routes/map.js
+++ server/routes/map.js
@@ -13,10 +13,10 @@ router.get('/', (req, res) => {
   }
   axios.request(url)
     .then(geo => {
-      console.log('\n***\nGeo Received from Google for address centering:\n', geo.data, 'latlng:', geo.data.results[0].geometry.location)
+      // console.log('\n***\nGeo Received from Google for address centering:\n', geo.data, 'latlng:', geo.data.results[0].geometry.location)
       searchPoint = geo.data.results[0].geometry.location
       const query = `SELECT
-      "name", "address", "location", ST_Distance_Sphere(ST_MakePoint(:longitude, :latitude), "location") AS distance
+      "name", "address", "location", "comment", ST_Distance_Sphere(ST_MakePoint(:longitude, :latitude), "location") AS distance
       FROM "storegeos"
       WHERE
       ST_Distance_Sphere(ST_MakePoint(:longitude, :latitude), "location") < :maxDistance
@@ -31,7 +31,7 @@ router.get('/', (req, res) => {
         type: Storegeo.sequelize.QueryTypes.SELECT
       })
       .then(geo => {
-        // console.log('\n\nWe got a response from store geo!\n', geo)
+        console.log('\n\n*****\nWe got a response from store geo!\n', geo)
         res.status(200).json({geo, searchPoint})
       })
       .catch(err => {
