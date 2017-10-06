f560dcfde26fe9dbcb19d1774720de7f1250f705
Neville Neville <nchallinger@gmail.com>
2017-10-06 18:50:33 +1100

    Adjust geolocation lookup to 25/sec max, adjust promise.all error return

--- server/routes/api.js
+++ server/routes/api.js
@@ -121,7 +121,7 @@ router.get('/order-form', (req, res) => {
 
 router.post('/store-geo', (req, res) => {
   let pThrottle = new PromiseThrottle({
-    requestsPerSecond: 50,
+    requestsPerSecond: 25,
     promiseImplementation: Promise
   })
   let geoRequests = []
@@ -131,7 +131,7 @@ router.post('/store-geo', (req, res) => {
     if (c.name) {
       let query = `${c.name},${c.street},${c.suburb},${c.state},${c.postcode}`.replace(/[ \t]/g, '+')
       let url = `https://maps.googleapis.com/maps/api/geocode/json?address=${query}&region=au&key=${config.GMAPS_API}`
-      console.log(`Comment for ${c.name}: ${c.comment}`)
+      // console.log(`Comment for ${c.name}: ${c.comment}`)
       storeNames.push({
         name: c.name.trim(),
         address: `${c.street},${c.suburb},${c.state}`,
@@ -145,6 +145,10 @@ router.post('/store-geo', (req, res) => {
   let geoFailures = []
   Storegeo.sync({force: true})
   Promise.all(geoRequests)
+    .catch(err => {
+      if (err) console.log('geoRequests promise error:', err)
+      return geoRequests
+    })
     .then(resList => {
       resList.forEach((res, i) => {
         if (res.data.status === 'OK') {
@@ -177,9 +181,7 @@ router.post('/store-geo', (req, res) => {
           }
         })
     })
-    .catch(err => {
-      if (err) console.log('geoRequests error:', err)
-    })
+
 })
 
 // router.get('/store-geo', (req, res) => {
